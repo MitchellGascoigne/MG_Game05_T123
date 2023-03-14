d1712ffdc8a42f9a350ae5b0ed9a5cd1eb1948b6
MitchellGascoigne <107447804+MitchellGascoigne@users.noreply.github.com>
2023-03-14 18:05:40 +1100

    Version 3
    - Update projectile script that registers two buttons pressed to spawn new colour based off of combinations

--- Assets/MG_Scripts/ProjectileController.cs
+++ Assets/MG_Scripts/ProjectileController.cs
@@ -2,35 +2,47 @@ using UnityEngine;
 
 public class ProjectileController : MonoBehaviour
 {
-    public GameObject greenFireballPrefab; // Reference to the green fireball prefab
-    public GameObject redFireballPrefab; // Reference to the red fireball prefab
-    public GameObject blueFireballPrefab; // Reference to the blue fireball prefab
-    public Transform firePoint; // Reference to the point where the fireball will spawn
-    public float fireballSpeed = 5f; // The initial speed of the fireball
-    public float fireballAcceleration = 2f; // The acceleration of the fireball
+    public GameObject greenFireballPrefab;
+    public GameObject redFireballPrefab;
+    public GameObject blueFireballPrefab;
+    public GameObject yellowFireballPrefab;
+    public GameObject cyanFireballPrefab;
+    public GameObject purpleFireballPrefab;
+    public Transform firePoint;
+    public float fireballSpeed = 30f;
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.G))
+        if (Input.GetKey(KeyCode.G) && Input.GetKey(KeyCode.R))
         {
-            // Instantiate a green fireball and set its velocity
-            GameObject greenFireball = Instantiate(greenFireballPrefab, firePoint.position, Quaternion.identity);
-            greenFireball.GetComponent<Rigidbody2D>().velocity = new Vector2(fireballSpeed, 0f);
-            fireballSpeed += fireballAcceleration;
+            Instantiate(yellowFireballPrefab, firePoint.position, Quaternion.identity)
+                .GetComponent<Rigidbody2D>().velocity = new Vector2(fireballSpeed, 0f);
+        }
+        else if (Input.GetKey(KeyCode.G) && Input.GetKey(KeyCode.B))
+        {
+            Instantiate(cyanFireballPrefab, firePoint.position, Quaternion.identity)
+                .GetComponent<Rigidbody2D>().velocity = new Vector2(fireballSpeed, 0f);
+        }
+        else if (Input.GetKey(KeyCode.R) && Input.GetKey(KeyCode.B))
+        {
+            Instantiate(purpleFireballPrefab, firePoint.position, Quaternion.identity)
+                .GetComponent<Rigidbody2D>().velocity = new Vector2(fireballSpeed, 0f);
+        }
+        else if (Input.GetKeyDown(KeyCode.G))
+        {
+            Instantiate(greenFireballPrefab, firePoint.position, Quaternion.identity)
+                .GetComponent<Rigidbody2D>().velocity = new Vector2(fireballSpeed, 0f);
         }
         else if (Input.GetKeyDown(KeyCode.R))
         {
-            // Instantiate a red fireball and set its velocity
-            GameObject redFireball = Instantiate(redFireballPrefab, firePoint.position, Quaternion.identity);
-            redFireball.GetComponent<Rigidbody2D>().velocity = new Vector2(fireballSpeed, 0f);
-            fireballSpeed += fireballAcceleration;
+            Instantiate(redFireballPrefab, firePoint.position, Quaternion.identity)
+                .GetComponent<Rigidbody2D>().velocity = new Vector2(fireballSpeed, 0f);
         }
         else if (Input.GetKeyDown(KeyCode.B))
         {
-            // Instantiate a blue fireball and set its velocity
-            GameObject blueFireball = Instantiate(blueFireballPrefab, firePoint.position, Quaternion.identity);
-            blueFireball.GetComponent<Rigidbody2D>().velocity = new Vector2(fireballSpeed, 0f);
-            fireballSpeed += fireballAcceleration;
+            Instantiate(blueFireballPrefab, firePoint.position, Quaternion.identity)
+                .GetComponent<Rigidbody2D>().velocity = new Vector2(fireballSpeed, 0f);
         }
     }
 }
+
