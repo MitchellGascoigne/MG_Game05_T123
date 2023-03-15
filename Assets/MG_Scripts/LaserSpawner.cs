fb2198f14c20a416f282dda43fa9f13458025f27
1026502 [home] <1026502@student.sae.edu.au>
2023-03-16 07:31:12 +1100

    16/03 Update 2
    -Fixed spawner compiling errors
    -Added game title

--- Assets/MG_Scripts/LaserSpawner.cs
+++ Assets/MG_Scripts/LaserSpawner.cs
@@ -24,12 +24,19 @@ public class LaserSpawner : MonoBehaviour
         {
             // Randomly select one of the laser prefabs
             int index = Random.Range(0, laserPrefabs.Length);
-            GameObject laserPrefab = laserPrefabs[index];
+           
+
+            Debug.Log("index chosen was: " + index);
+
+
+            // ==== Commented out and ammended script due to compiling errors.
+
+            //GameObject laserPrefab = laserPrefabs[index];    
 
             // Spawn the laser at the specified Y position and a random X position just outside the camera
             float spawnX = Camera.main.ViewportToWorldPoint(new Vector3(1.1f, 0f, 0f)).x;
             Vector3 spawnPos = new Vector3(spawnX, spawnY, 0f);
-            Instantiate(laserPrefab, spawnPos, Quaternion.identity);
+            Instantiate(laserPrefabs[index], spawnPos, Quaternion.identity);
 
             // Set the next spawn time
             nextSpawnTime = Time.time + Random.Range(minSpawnDelay, maxSpawnDelay);
