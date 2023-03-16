fc69448c9c7c6a9cb5748b0c6221b82fa5c8d934
Mitchell Gascoigne <107447804+MitchellGascoigne@users.noreply.github.com>
2023-03-16 14:07:08 +1100

    Animations
    - Flame Hovercar
    - Laser Script UPDATE

--- Assets/MG_Scripts/LaserSpawner.cs
+++ Assets/MG_Scripts/LaserSpawner.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class LaserSpawner : MonoBehaviour
 {
-    public GameObject[] laserPrefabs;
+    public GameObject[] Lasers; 
     public float minSpawnDelay = 2f;
     public float maxSpawnDelay = 8f;
     public float spawnY = 0f;
@@ -23,7 +23,7 @@ public class LaserSpawner : MonoBehaviour
         if (Time.time >= nextSpawnTime)
         {
             // Randomly select one of the laser prefabs
-            int index = Random.Range(0, laserPrefabs.Length);
+            int index = Random.Range(0, Lasers.Length);
            
 
             Debug.Log("index chosen was: " + index);
@@ -36,7 +36,7 @@ public class LaserSpawner : MonoBehaviour
             // Spawn the laser at the specified Y position and a random X position just outside the camera
             float spawnX = Camera.main.ViewportToWorldPoint(new Vector3(1.1f, 0f, 0f)).x;
             Vector3 spawnPos = new Vector3(spawnX, spawnY, 0f);
-            Instantiate(laserPrefabs[index], spawnPos, Quaternion.identity);
+            Instantiate(Lasers[index], spawnPos, Quaternion.identity);
 
             // Set the next spawn time
             nextSpawnTime = Time.time + Random.Range(minSpawnDelay, maxSpawnDelay);
